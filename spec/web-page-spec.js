55a02ce0d31939f25294cad8c98721f1f38353eb
Dem Pilafian <dem@centerkey.com>
2020-10-11 02:17:15 -0700

    Replace deprecated deepEqual() with deepStrictEqual()

--- spec/web-page-spec.js
+++ spec/web-page-spec.js
@@ -1,7 +1,7 @@
 // Mocha Specification Cases
 
 // Imports
-const assert =          require('assert').strict;
+const assert =          require('assert');
 const serverListening = require('server-listening');
 const { JSDOM } =       require('jsdom');
 
@@ -27,13 +27,13 @@ describe('The web page', () => {
    it('has the correct URL -> ' + url, () => {
       const actual =   { url: dom.window.location.href };
       const expected = { url: url };
-      assert.deepEqual(actual, expected);
+      assert.deepStrictEqual(actual, expected);
       });
 
    it('has the correct title', () => {
       const actual =   { title: dom.window.document.title };
       const expected = { title: 'DataDashboard' };
-      assert.deepEqual(actual, expected);
+      assert.deepStrictEqual(actual, expected);
       });
 
    it('has exactly one header, main, and footer', () => {
@@ -44,7 +44,7 @@ describe('The web page', () => {
          footer: $('body >footer').length,
          };
       const expected = { header: 1, main: 1, footer: 1 };
-      assert.deepEqual(actual, expected);
+      assert.deepStrictEqual(actual, expected);
       });
 
    });
@@ -58,7 +58,7 @@ describe('The document content', () => {
       const html = dom.window.document.documentElement.outerHTML;
       const actual =   { '🚀': !!html.match(/🚀/g), '🪐': !!html.match(/🪐/g) };
       const expected = { '🚀': false,               '🪐': false };
-      assert.deepEqual(actual, expected);
+      assert.deepStrictEqual(actual, expected);
       });
 
    });
