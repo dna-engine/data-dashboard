dcb544002e91562b67e10e8100507f67bb7f7463
dpilafian <dem@centerkey.com>
2021-02-24 02:28:45 -0800

    Release v0.1.5

--- src/web-server/index.ts
+++ src/web-server/index.ts
@@ -5,17 +5,23 @@ import express from 'express';
 import { Server } from 'http';
 import { AddressInfo } from 'net';
 
+const webServerDefault = {
+   folder: 'docs',
+   port:   7531,
+   };
+
 const webServer = {
    start(): Server {
-      const webFolder = () => process.env.webFolder || 'docs';
-      const port = () =>      process.env.port || 7531;
+      const folder = () => process.env.webFolder || webServerDefault.folder;
+      const port = () =>   process.env.port || webServerDefault.port;
       const devMode = {
          setHeaders: (response: express.Response) => response.setHeader('Connection', 'close'),  //disable Keep-Alive for jsdom
          etag:       false  //always server fresh files (avoids 304 Not Modified for html files)
          };
-      const webServerInst = express().use(express.static(webFolder(), devMode)).listen(port());
+      const webServerInst = express().use(express.static(folder(), devMode)).listen(port());
       const msg = { start: '  --- server listening on port:', close: '  --- server shutdown' };
-      webServerInst.on('listening', () => console.log(msg.start, (<AddressInfo>webServerInst.address()).port, webFolder()));
+      const getInfo = () => <AddressInfo>webServerInst.address();
+      webServerInst.on('listening', () => console.log(msg.start, getInfo().port, folder()));
       webServerInst.on('close',     () => console.log(msg.close));
       return webServerInst;
       },
