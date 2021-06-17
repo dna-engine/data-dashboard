8c6705f408db5a2bd6f954a8d81d7c70acafd5e1
dpilafian <dem@centerkey.com>
2021-06-16 23:15:56 -0700

    Patch google books api to use https protocol

--- src/web/widgets/spacex-books/spacex-books.ts
+++ src/web/widgets/spacex-books/spacex-books.ts
@@ -5,32 +5,36 @@ import { dna } from 'dna.js';
 import { fetchJson } from 'fetch-json';
 import { app } from '../../ts/app';
 
-type WidgetModel = {
-   books: {
-      volumeInfo: {
-         title:       string,
-         imageLinks:  { thumbnail: string },
-         previewLink: string,
-         publisher:   string,
-         },
-      saleInfo: {
-         listPrice: { amount: number },
-         },
-      }[],
-   };
 type RawData = {
-   items: WidgetModel['books'],
+   items: Book[],
+   };
+type WidgetModel = {
+   books: Book[],
+   };
+type Book = {
+   volumeInfo: {
+      title:       string,
+      imageLinks:  { thumbnail: string },
+      previewLink: string,
+      publisher:   string,
+      },
+   saleInfo: {
+      listPrice: { amount: number },
+      },
    };
 
 const appWidgetSpacexBooks = {
    show(widgetElem: JQuery): void {
       const url = 'https://www.googleapis.com/books/v1/volumes';
       const params = { q: 'spacex' };
+      const hasCover = (book: Book) => !!book.volumeInfo.imageLinks;
+      const fixHttpProtocol = (book: Book) => book.volumeInfo.imageLinks.thumbnail =
+         book.volumeInfo.imageLinks.thumbnail.replace('http:', 'https:');
       const handleData = (data: RawData) => {
          app.util.spinnerStop(widgetElem);
-         data.items = data.items.filter(book => book.volumeInfo.imageLinks);
          const model = <WidgetModel>dna.getModel(widgetElem);
-         model.books = data.items;
+         model.books = data.items.filter(hasCover);
+         model.books.forEach(fixHttpProtocol);
          dna.refresh(widgetElem);
          };
       app.util.spinnerStart(widgetElem);
