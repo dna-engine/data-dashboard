7e9cd73d7556dfcba4c199f2d38f1d622bf16fc7
dpilafian <dem@centerkey.com>
2021-10-24 01:16:38 -0700

    Release v0.2.2

--- src/models/types.ts
+++ src/models/types.ts
@@ -2,5 +2,4 @@
 
 export type Json = string | number | boolean | null | undefined | JsonObject | Json[];
 export type JsonObject = { [key: string]: Json };
-export type JsonArray = Json[];
 export type JsonData = JsonObject | Json[];
