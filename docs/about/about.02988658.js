3d70321752aa7103dee05de413bd9a55dc30c264
dpilafian <dem@centerkey.com>
2024-01-27 04:12:53 -0800

    Release v0.5.1

--- docs/about/about.02988658.js
+++ docs/about/about.02988658.js
@@ -1,2 +1,2 @@
-//! data-dashboard v0.5.0 ~~ https://data-dashboard.js.org ~~ MIT License
+//! data-dashboard v0.5.1 ~~ https://data-dashboard.js.org ~~ MIT License
 var about=function(){"use strict";const about={setup(){console.log("DataDashboard");console.log(">>> About page")}};about.setup();return about}();
