e3dc183cbb1f4b1e79d9ca3b41690c1e45be72cf
pilafmon <pilaf@centerkey.com>
2025-10-21 03:23:14 -0700

    Release v0.5.6

--- docs/about/about.533c1acf.js
+++ docs/about/about.533c1acf.js
@@ -1,2 +1,2 @@
-//! data-dashboard v0.5.5 ~~ https://data-dashboard.js.org ~~ MIT License
+//! data-dashboard v0.5.6 ~~ https://data-dashboard.js.org ~~ MIT License
 var about=function(){"use strict";const about={setup(){console.info("DataDashboard");console.info(">>> About page")}};about.setup();return about}();
