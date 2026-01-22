9f742863e44cced34af8a337f43ba0cd4aac33a7
pilafmon <pilaf@centerkey.com>
2026-01-22 12:00:50 -0800

    Switch to github actions to publish website

--- docs/about/about.533c1acf.js
+++
@@ -1,2 +0,0 @@
-//! data-dashboard v0.5.6 ~~ https://data-dashboard.js.org ~~ MIT License
-var about=function(){"use strict";const about={setup(){console.info("DataDashboard");console.info(">>> About page")}};about.setup();return about}();
